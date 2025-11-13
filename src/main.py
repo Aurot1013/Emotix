a2feba844558f82f2b674a7cba5d07ce8a9bba7b
Aurot1013 <aurelien.berlot1013@gmail.com>
2025-11-13 22:25:05 +0100

    Nettoyage regex, Tokenizer, Sequence, Padding

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
 import pandas as pd
+import re
+import unicodedata
 
-data = pd.read_csv("data/examples.csv");
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
 
-print (data.head());
+
+
+
+data = pd.read_csv("data/examples.csv") #Recupere le tableaux
+
+
+# Enleve les accent avec unicode, enleve les majuscules avec lower et applique grace a apply
+textFinal = data["text"].apply(
+    lambda x: "".join(
+        c for c in unicodedata.normalize("NFD", re.sub(r"[^\w\s]", "", x.lower()))
+        if unicodedata.category(c) != "Mn"
+    )
+)
+#Tokenizer 
+x= textFinal
+y = data["label"]
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(x) #Construit le vocabulaire et les indices correpondants a chaque mot
+sequences = tokenizer.texts_to_sequences(x) #Transforme chaque phrase en une liste d'indice numérique
+
+longueur_max = max(len(seq) for seq in sequences); #Recupere la phrase la plus longue
+X_padded = pad_sequences(sequences, maxlen= longueur_max);#Ajoute des 0 jusqu a la phrase la plus longue car en ML on a besoin d avoir la meme taile de caractere
+
+
+print(y);
